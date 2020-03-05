ee04aa1ed05e023384f8dd9777ca39ee5cd9c0cf
Diego <loidiegopes@gmail.com>
2020-03-05 22:24:59 +0100

    Add 'branch-name' argument

--- git-terminate.py
+++ git-terminate.py
@@ -4,3 +4,5 @@ parser = argparse.ArgumentParser(
     prog='git-terminate',
     description='Checkout to master, pull, delete a branch and refresh remote refs.'
 )
+
+parser.add_argument('branch_name', metavar='branch-name', help='the branch to be deleted')
