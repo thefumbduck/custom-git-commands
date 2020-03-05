43b3af840312f88b9f993aa34e7ae449a56e59b2
Diego <loidiegopes@gmail.com>
2020-03-05 16:14:25 +0100

    Push branch with given parameters

--- git-publish.py
+++ git-publish.py
@@ -1,4 +1,5 @@
 import argparse
+import subprocess
 
 parser = argparse.ArgumentParser(
         prog='git-publish',
@@ -7,3 +8,7 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument('branch_name', metavar='branch-name', action='store', help='the branch to be published')
 parser.add_argument('--remote', action='store', default='origin', help='the remote the branch will be published to')
+
+args = parser.parse_args()
+
+subprocess.run('git push --set-upstream'.split() + [args.branch_name, args.remote])
