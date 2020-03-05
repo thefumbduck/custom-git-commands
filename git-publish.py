8cce366a89b7d7f64a3847c85034f9bd5b76c31d
Diego <loidiegopes@gmail.com>
2020-03-05 15:59:50 +0100

    Add branch-name to list of arguments

--- git-publish.py
+++ git-publish.py
@@ -1,3 +1,8 @@
 import argparse
 
-parser = argparse.ArgumentParser(prog='git-publish', description="Publish a branch remotely. Equivalent of 'git push --set-upstream origin {branch-name}'.")
+parser = argparse.ArgumentParser(
+        prog='git-publish',
+        description="Publish a branch remotely. Equivalent of 'git push --set-upstream origin {branch-name}'."
+)
+
+parser.add_argument('branch-name', action='store', help='the branch to be published')
