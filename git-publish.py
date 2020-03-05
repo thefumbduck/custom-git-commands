c33a1eadd57a54f2f77b3ab58393dff2d8471022
Diego <loidiegopes@gmail.com>
2020-03-05 16:10:02 +0100

    Make parser.branch_name usable

--- git-publish.py
+++ git-publish.py
@@ -5,5 +5,5 @@ parser = argparse.ArgumentParser(
         description="Publish a branch remotely. Equivalent of 'git push --set-upstream origin {branch-name}'."
 )
 
-parser.add_argument('branch-name', action='store', help='the branch to be published')
+parser.add_argument('branch_name', metavar='branch-name', action='store', help='the branch to be published')
 parser.add_argument('--remote', action='store', default='origin', help='the remote the branch will be published to')
