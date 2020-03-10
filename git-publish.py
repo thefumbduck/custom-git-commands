54c21de656360ba1b119bcca907af32eebadff71
thefumbduck <loidiegopes@gmail.com>
2020-03-10 21:39:34 +0100

    Make branch-name optional

--- git-publish.py
+++ git-publish.py
@@ -9,7 +9,7 @@ parser = argparse.ArgumentParser(
         description="Publish a branch remotely. Equivalent of 'git push --set-upstream origin {branch-name}'."
 )
 
-parser.add_argument('branch_name', metavar='branch-name', action='store', help='the branch to be published')
+parser.add_argument('branch_name', metavar='branch-name', nargs='?', default=get_current_branch_name(), help='the branch to be published')
 parser.add_argument('--remote', action='store', default='origin', help='the remote the branch will be published to')
 
 args = parser.parse_args()
